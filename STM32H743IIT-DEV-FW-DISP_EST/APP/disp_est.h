b56a4c602b367aa381a9d07e3694418ca6c33978
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2024-03-26 22:28:04 +0800

    auto commit by win-upload.bat

--- STM32H743IIT-DEV-FW-DISP_EST/APP/disp_est.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DISP_EST_H
-#define __DISP_EST_H
-
-int disp_est(void);
-
-#endif /* __DISP_EST_H */
--- STM32H743IIT-DEV-FW-DISP_EST/APP/global_statement.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GLOBAL_STATEMENT_H
-#define __GLOBAL_STATEMENT_H
-
-#define SUCCESS 0
-#define FAIL 1
-
-#endif /* __GLOBAL_STATEMENT_H */
